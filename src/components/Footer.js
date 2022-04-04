e2f1ea867f8561f184d1da7be595e7108e761138
Eric Phifer <ericoncode@gmail.com>
2022-04-04 10:01:46 -0600

    begin altering footer and nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,35 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import codepen from '../img/social/codepen.svg'
+import github from '../img/social/github.svg'
 
 const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        {/* <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div> */}
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="has-background-black has-text-white-ter">
             <div style={{ maxWidth: '80vw', margin: '0 auto' }} className="columns">
               <div className="column is-4">
                 <section className="menu">
                   <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
+                    <li id="copyright">
+                        &copy; Eric Phifer LLC {new Date().getFullYear()}
                     </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
+                    <li id="developer">
+                      <a href="https://ericphifer.com" target="_blank" rel="noreferrer">
+                        Designed &amp; Developed by Eric Phifer LLC
                       </a>
+                    </li>
+                    <li id="privacy">
+                      <div />
+                      <a href="https://ericphifer.com/privacypolicy" target="_blank" rel="noreferrer">Privacy Policy</a> |{' '}
+                      <a href="https://ericphifer.com/termsconditions" target="_blank" rel="noreferrer">Terms &amp; Conditions</a>
                     </li>
                   </ul>
                 </section>
               </div>
-              <div className="column is-4">
+              <div className="column is-2">
                 <section>
                   <ul className="menu-list">
                     <li>
@@ -53,48 +37,28 @@ const Footer = class extends React.Component {
                         Latest Stories
                       </Link>
                     </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
                   </ul>
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" target="_blank" rel="noreferrer" href="https://facebook.com">
+                <a title="codepen" target="_blank" rel="noreferrer" href="https://codepen.io/Eric-Phifer">
                   <img
-                    src={facebook}
-                    alt="Facebook"
+                    src={codepen-brands}
+                    alt="CodePen"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" target="_blank" rel="noreferrer" href="https://twitter.com">
+                <a title="github" target="_blank" rel="noreferrer" href="https://github.com/EricPhifer">
                   <img
                     className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" target="_blank" rel="noreferrer" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" target="_blank" rel="noreferrer" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
+                    src={github}
+                    alt="GitHub"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
               </div>
             </div>
           </div>
-          <p style={{paddingBottom:'1em', color:'#7c7c7c'}}>Copyright © yourdomain.com | 20XX</p>
         </div>
       </footer>
     )
