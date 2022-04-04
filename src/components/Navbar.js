a2161982aec195b5ae7afe16f974fd9acd0f76ed
Eric Phifer <ericoncode@gmail.com>
2022-04-04 12:16:04 -0600

    recreate navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import GitHubButton from 'react-github-btn'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -11,73 +10,86 @@ const Navbar = class extends React.Component {
     }
   }
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
-      }
-    )
-  }
+  // toggleHamburger = () => {
+  //   // toggle the active boolean in the state
+  //   this.setState(
+  //     {
+  //       active: !this.state.active,
+  //     },
+  //     // after state has been updated,
+  //     () => {
+  //       // set the class in state for the navbar accordingly
+  //       this.state.active
+  //         ? this.setState({
+  //             navBarActiveClass: 'is-active',
+  //           })
+  //         : this.setState({
+  //             navBarActiveClass: '',
+  //           })
+  //     }
+  //   )
+  // }
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container flex-around">
-          <div className="navbar-brand logo">
-            <Link to="/" className="navbar-item" title="Logo">
-              <h1>Brand | Logo</h1>
-            </Link>
-              <GitHubButton href="https://github.com/simarmannsingh">Follow</GitHubButton>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-              onKeyDown={() => this.toggleHamburger()}
-              role="button" tabIndex="0"
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">            
-            <Link className="navbar-item" to="/blog">
+      <div>
+        <nav className="navStyles">
+          <ul>
+            <li className="navLink">
+              <a href="https://ericphifer.com/about" rel="noreferrer" >About</a>
+            </li>
+            <li className="navLink">
+              <Link to="/" rel="noreferrer">
                 Blog
               </Link>
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>              
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>           
+            </li>
+            <li id="logo" className="navLink">
+              <a href="https://ericphifer.com" rel="noreferrer">
+                <div id="logo" />
+              </a>
+            </li>
+            <li className="navLink">
+              <a href="https://services.ericphifer.com"  rel="noreferrer">
+                Services
+              </a>
+            </li>
+            <li className="navLink">
+              <a href="https://ericphifer.com/contact" rel="noreferrer">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <nav className="mobileNavStyles">
+          <div id="menuToggle">
+            <input type="checkbox" />
+            <span />
+            <span />
+            <span />
+            <ul id="menu">
+              <li className="mobileLink">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="mobileLink">
+                <a href="https://blog.ericphifer.com" rel="noreferrer">
+                  Blog
+                </a>
+              </li>
+              <li className="mobileLink">
+                <a href="https://services.ericphifer.com" rel="noreferrer">
+                  Services
+                </a>
+              </li>
+              <li className="mobileLink">
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li className="mobileLink">
+                <Link to="/">
+                  <div id="mobileLogo" />
+                </Link>
+              </li>
+            </ul>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     )
   }
 }
