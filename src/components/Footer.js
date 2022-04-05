100deb82366c4b6b8f185410612a182d4ad93275
Eric Phifer <ericoncode@gmail.com>
2022-04-05 06:48:40 -0600

    adjust Footer to align properly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,17 +12,19 @@ const Footer = class extends React.Component {
             <div style={{ margin: '0 auto' }} className="columns">
                 <section className="menu column is-4">
                   <ul className="menu-list" style={{ margin: "0" }}>
-                    <li id="copyright">
+                    <li>
                         &copy; Eric Phifer LLC {new Date().getFullYear()}
                     </li>
-                    <li id="developer">
+                    <li>
                       <a href="https://ericphifer.com" target="_blank" rel="noreferrer">
                         Designed &amp; Developed by Eric Phifer LLC
                       </a>
                     </li>
-                    <li id="privacy">
-                      <a href="https://ericphifer.com/privacypolicy" target="_blank" rel="noreferrer">Privacy Policy</a><span> | </span>
-                      <a href="https://ericphifer.com/termsconditions" target="_blank" rel="noreferrer">Terms &amp; Conditions</a>
+                    <li>
+                      <span>
+                        <a href="https://ericphifer.com/privacypolicy" target="_blank" rel="noreferrer">Privacy Policy</a> | 
+                        <a href="https://ericphifer.com/termsconditions" target="_blank" rel="noreferrer">Terms &amp; Conditions</a>
+                      </span>
                     </li>
                   </ul>
                 </section>
